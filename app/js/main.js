9c2601beff71259af60ec5f9e738ae46f31d3af1
Michael Marcinek <michaelmarcinek@gmail.com>
2015-07-06 16:16:25 -0400

    Adds main view, starts navbar

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,19 @@
 
   angular.module('AutTrak', ['ui-router', 'ngMaterial'])
 
-  .config( function ($mdThemingProvider){
+  .config( function ($mdThemingProvider, $stateProvider, $urlRouterProvider){
     $mdThemingProvider.theme('default')
     .primaryPalette('blue grey')
     .accentPalette('blue');
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('main', {
+    url: '/',
+    templateUrl: 'js/main/main.tpl.html'
+  });
+
 
   });
 
---
+++ app/js/main/main.controller.js
@@ -0,0 +1,16 @@
+;(function (){
+
+  'use strict';
+
+  angular.module('AutoTrak')
+
+  .controller('Main', [ '$scope',
+
+    function ($scope){
+
+
+    }
+
+  ]);
+
+}());

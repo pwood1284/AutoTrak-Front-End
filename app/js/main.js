c6cd132deef655807b7dda6ced4073df5ab3af88
Michael Marcinek <michaelmarcinek@gmail.com>
2015-07-06 15:55:57 -0400

    Sets up project and color themes

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
-console.log('Angular Rocks', angular);
+;(function (){
+
+  'use strict';
+
+  angular.module('AutTrak', ['ui-router', 'ngMaterial'])
+
+  .config( function ($mdThemingProvider){
+    $mdThemingProvider.theme('default')
+    .primaryPalette('blue grey')
+    .accentPalette('blue');
+
+  });
+
+}());

75b2e909ad02da190a0d2a8c89e72cc4bf5e2d55
pwood1284 <pwood1284@gmail.com>
2015-08-24 20:52:47 -0400

    Updated keypad

--- app/js/user/user.controller.js
+++ app/js/user/user.controller.js
@@ -16,23 +16,25 @@
 
       var vm = this;
       // var em = self;
-
         vm.employee_pin = '';
-        vm.keys = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        vm.keys = [1,2,3,4,5,6,7,8,9];
         vm.onKeyPressed = onKeyPressed;
 
         function onKeyPressed(data) {
             if (data == '<') {
-                vm.employee_pin = vm.employee_pin.slice(0, vm.employee_pin.length - 1);
+              vm.employee_pin = '';
+                // vm.employee_pin = vm.employee_pin.slice(0, vm.employee_pin.length - 1);
             } else {
                 vm.employee_pin += data;
             }
         }
 
-
       $scope.keypadEnt = function (employee_pin){
         UserService.loginTech(employee_pin);
       };
+      // $scope.keypadEnt = function (employee_pin){
+      //   UserService.loginTech(employee_pin);
+      // };
 
       // UserService.getTechRO().success( function (data){
       //   console.log(data);
